a48deed5ad7b08dec6f6c59bd1871bae946e9148
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-15 19:06:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#D32F2F",
+        articleBg: "rgba(255, 255, 255, 0.95)",
+        headingText: "#B71C1C",
+        descText: "#424242",
+        authorText: "#757575",
+      },
+    },
   },
   plugins: [],
 }
-
