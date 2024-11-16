a521db0359b75dc597113edbe28f42f7a7450fa7
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-16 15:03:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import Navbar from "./components/Navbar"
+import { useState } from 'react'
+import './index.css'
+import './App.css'
+import Navbar from './components/Navbar'
+import NewsDashboard from './components/NewsDashboard'
+import Footer from './components/Footer'
 
 function App() {
-
   return (
     <>
       <Navbar />
+      <NewsDashboard />
+      <Footer />
     </>
   )
 }
